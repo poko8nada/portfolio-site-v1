3ecbb9f9f125532cc7ef9accc91b2934d72589ef
poko8nada <57497035+poko8nada@users.noreply.github.com>
2024-12-09 17:36:14 +0900

    Add #5-29 postページ作成・スタイリング

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,10 +1,43 @@
 import Link from 'next/link';
+import Image from 'next/image';
+import styles from './posts.module.css';
+import PostStyles from './post.module.css';
 
+import { getPosts } from '@/src/lib/post';
+const allPosts = getPosts();
+console.log(allPosts);
 export default () => {
   return (
-    <div>
-      <h1>POSTS</h1>
-      <Link href="/">HOME</Link>
-    </div>
+    <>
+      <div style={{ textAlign: 'center' }}>
+        <p>webディレクター兼エンジニア兼マーケターの雑記です。</p>
+      </div>
+      <div className={styles.container}>
+        {allPosts
+          .filter((post) => post !== null)
+          .map((post) => {
+            const data = post.formattedData;
+            return (
+              <article key={post.slug} className={PostStyles.post_container}>
+                <Link href={`/posts/${post.slug}`} className={PostStyles.post}>
+                  <div className={PostStyles.text}>
+                    <p>{data.title}</p>
+                    <small>{data.createdAt}</small>
+                  </div>
+                  <Image
+                    src={data.thumbnail}
+                    alt={''}
+                    style={{ objectFit: 'cover' }}
+                    width={160}
+                    height={160}
+                    layout="intrinsic"
+                    sizes="(max-width: 400px)  160px, 200px"
+                  />
+                </Link>
+              </article>
+            );
+          })}
+      </div>
+    </>
   );
 };
