649bbaabd7471d7eee2f7c7f9336a72414ddc38a
poko8nada <57497035+poko8nada@users.noreply.github.com>
2024-12-09 17:35:08 +0900

    Update #5-29 設定の調整

--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,21 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/posts',
+        permanent: false,
+      },
+    ]
+  },
+}
+module.exports = {
+  images: {
+    domains: ['picsum.photos'],
+  },
+};
+
 export default nextConfig;
