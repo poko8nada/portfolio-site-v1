2d22b0b1ee7b758cae749e7b67b067ee0035ec94
poko8nada <57497035+poko8nada@users.noreply.github.com>
2024-12-14 10:25:22 +0900

    Add home/about画面作成。

--- src/app/(header)/page.tsx
+++ src/app/(header)/page.tsx
@@ -4,30 +4,61 @@ import PostsCard from '@/src/components/posts_card/page';
 import SectionBody from '@/src/components/section_body/page';
 import SectionHeader from '@/src/components/section_header/page';
 import Button from '@/src/components/ui/button/page';
+import Image from 'next/image';
 export default function Home() {
   const topPosts = getTopPosts();
   return (
     <>
       <SectionBody>
         <SectionHeader>about</SectionHeader>
-      </SectionBody >
+        <p>webディレクター兼エンジニア兼マーケターです。</p>
+        <div style={{ display: 'flex', gap: '1.25em', marginTop: '1rem' }}>
+          <Link
+            style={{
+              borderRadius: '22%',
+              overflow: 'hidden',
+              padding: '.7rem',
+              background: '#1b2da3',
+            }}
+            href="https://x.com/you88451h"
+            target="_blank"
+          >
+            <Image src="/images/x-logo.svg" alt="" width={30} height={30} />
+          </Link>
+          <Link
+            style={{
+              borderRadius: '22%',
+              overflow: 'hidden',
+              padding: '.7rem',
+              background: '#1b2da3',
+            }}
+            href="https://github.com/poko8nada"
+            target="_blank"
+          >
+            <Image
+              src="/images/github-mark-white.svg"
+              alt=""
+              width={30}
+              height={30}
+            />
+          </Link>
+        </div>
+      </SectionBody>
       <SectionBody>
         <SectionHeader>recent posts</SectionHeader>
-        {
-          topPosts.map(({ slug, formattedData }) => {
-            return (
-              <PostsCard key={slug} slug={slug} formattedData={formattedData} />
-            )
-          })
-        }
+        {topPosts.map(({ slug, formattedData }) => {
+          return (
+            <PostsCard key={slug} slug={slug} formattedData={formattedData} />
+          );
+        })}
         <div style={{ marginTop: '2rem' }}>
-          <Button href="/posts" >read more</Button>
+          <Button href="/posts">read more</Button>
         </div>
-      </SectionBody >
+      </SectionBody>
       <SectionBody>
         <SectionHeader>works</SectionHeader>
         <p style={{ padding: '1rem' }}>Under construction ...</p>
-      </SectionBody >
+      </SectionBody>
     </>
   );
 }
