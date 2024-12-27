128664601b095d77eca4cd88dffe06c82ab9d455
poko8nada <57497035+poko8nada@users.noreply.github.com>
2024-12-27 16:09:11 +0900

    Update ブログヘッダーに更新日を追加するロジック導入。公開日と更新日のicon追加。mdにversion追加。1219の記事を一部修正

--- src/components/post_header/page.tsx
+++ src/components/post_header/page.tsx
@@ -1,18 +1,22 @@
 import styles from './post_header.module.css';
-import Image from 'next/image';
 import type { Post } from '@/src/lib/post';
 import PostThumbnail from '@/src/components/ui/post_thumbnail/page';
+import Image from 'next/image';
 
 export default ({ post }: { post?: Post }) => {
   const title = post?.formattedData.title;
   const createdAt = post?.formattedData.createdAt;
+  const updatedAt = post?.formattedData.updatedAt;
   const thumbnail = post?.formattedData.thumbnail;
 
   return (
     <div className={styles.article_header}>
       <h2>{title}</h2>
       <PostThumbnail thumbnail={thumbnail} size={70} />
-      <p>Created at {createdAt}</p>
+      <p>
+        <span>{createdAt}</span>
+        {updatedAt === createdAt ? null : <span>{updatedAt}</span>}
+      </p>
     </div>
   );
 };
