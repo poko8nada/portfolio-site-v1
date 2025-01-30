1d6bd793f6268062a71efa2e17432e3c631d7d61
poko8nada <57497035+poko8nada@users.noreply.github.com>
2025-01-30 16:54:28 +0900

    Improve post styling

--- src/components/postBody.tsx
+++ src/components/postBody.tsx
@@ -1,12 +1,15 @@
 import Markdown from 'react-markdown'
 import 'github-markdown-css'
+import remarkGfm from 'remark-gfm'
 
 import Post_embed from '@/components/ui/postEmbed'
 
 export default ({ content }: { content: string }) => {
   return (
-    <article className='markdown-body article_container rounded-lg py-20 px-5 md:px-12 w-full'>
-      <Markdown components={{ code: Post_embed }}>{content}</Markdown>
+    <article className='markdown-body article_container rounded-lg py-20 md:px-12 w-full'>
+      <Markdown components={{ code: Post_embed }} remarkPlugins={[remarkGfm]}>
+        {content}
+      </Markdown>
     </article>
   )
 }
